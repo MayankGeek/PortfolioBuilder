7f2a798a20ebf8c71daccc7b7c43ef8d95f0a69a
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-04-24 03:12:47 +0530

    added frontend (need to change page design according to data)

---
+++ PortfolioBuilder/Home/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+app_name="Home"
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('get_details_form/',views.get_details_form,name='get_details_form'),
+    path('details_page/',views.details_page,name='details_page'),
+    path('add_skills_form/',views.add_skills_form,name='add_skills_form'),
+    path('skill_details_page/',views.skill_details_page,name='skill_details_page'),
+    path('add_education_form/',views.add_education_form,name='add_education_form'),
+    path('education_details_page/',views.education_details_page,name='education_details_page'),
+    path('add_expertise_form/',views.add_expertise_form,name='add_expertise_form'),
+    path('expertise_details_page/',views.expertise_details_page,name='expertise_details_page'),
+    path('add_experience_form/',views.add_experience_form,name='add_experience_form'),
+    path('experience_details_page/',views.experience_details_page,name='experience_details_page'),
+    path('add_project_form/',views.add_project_form,name='add_project_form'),
+    path('project_details_page/',views.project_details_page,name='project_details_page'),
+]
--- PortfolioBuilder/Home/views.py
+++ PortfolioBuilder/Home/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def get_details_form(request):
+    return render(request,'get_details_form.html')
+
+def details_page(request):
+    return render(request,'details_page.html')
+
+def add_skills_form(request):
+    return render(request,'add_skills_form.html')
+
+def skill_details_page(request):
+    return render(request,'skill_details.html')
+
+def add_education_form(request):
+    return render(request,'add_education_form.html')
+
+def education_details_page(request):
+    return render(request,'education_details.html')
+
+def add_expertise_form(request):
+    return render(request,'add_expertise_form.html')
+
+def expertise_details_page(request):
+    return render(request,'expertise_details.html')
+
+def add_experience_form(request):
+    return render(request,'add_experience_form.html')
+
+def experience_details_page(request):
+    return render(request,'experience_details.html')
+
+def add_project_form(request):
+    return render(request,'add_project_form.html')
+
+def project_details_page(request):
+    return render(request,'project_details.html')
