15b3766b47d69e675c4d7f4656da493c2e7a5082
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-06-21 00:42:09 +0530

    added trusted csrf in settings_rail.py

--- PortfolioBuilder/PortfolioBuilder/settings_rail.py
+++ PortfolioBuilder/PortfolioBuilder/settings_rail.py
@@ -4,6 +4,8 @@ import os
 
 SECRET_KEY=config('SECRET_KEY')
 ALLOWED_HOSTS = ['portfoliobuilder-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://portfoliobuilder-production.up.railway.app/']
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
