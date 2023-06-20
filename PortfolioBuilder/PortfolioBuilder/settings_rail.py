bc05e6b234b6c6bf4c892b09de1b4e3ab4837832
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-06-20 23:58:31 +0530

    settings_rail.py whitenoise conf

--- PortfolioBuilder/PortfolioBuilder/settings_rail.py
+++ PortfolioBuilder/PortfolioBuilder/settings_rail.py
@@ -2,17 +2,3 @@ from PortfolioBuilder.settings import *
 from decouple import config
 SECRET_KEY=config('SECRET_KEY')
 ALLOWED_HOSTS = ['portfoliobuilder-production.up.railway.app']
-
-INSTALLED_APPS = [
-    'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',    
-]
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
