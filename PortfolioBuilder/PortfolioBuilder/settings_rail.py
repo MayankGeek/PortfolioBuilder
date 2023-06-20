0611dec47e31142632cbd7ae2ccfb8d4a0d1fe1e
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-06-20 23:47:27 +0530

    allowed host added in settings_Rail.py(removed slash)

--- PortfolioBuilder/PortfolioBuilder/settings_rail.py
+++ PortfolioBuilder/PortfolioBuilder/settings_rail.py
@@ -1,4 +1,4 @@
 from PortfolioBuilder.settings import *
 from decouple import config
 SECRET_KEY=config('SECRET_KEY')
-ALLOWED_HOSTS = ['portfoliobuilder-production.up.railway.app/']
+ALLOWED_HOSTS = ['portfoliobuilder-production.up.railway.app']
