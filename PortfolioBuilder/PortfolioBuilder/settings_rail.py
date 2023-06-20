ee67133956a3815e74d18fbffa07d617d51b96bd
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-06-21 00:29:25 +0530

    changed default db to postgres live db

--- PortfolioBuilder/PortfolioBuilder/settings_rail.py
+++ PortfolioBuilder/PortfolioBuilder/settings_rail.py
@@ -8,3 +8,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'HOST': config('DATABASE_HOST'),
+            'PORT': config('DATABASE_PORT'),
+            'OPTIONS': {'sslmode': 'require'},
+        }
+}
+ 
