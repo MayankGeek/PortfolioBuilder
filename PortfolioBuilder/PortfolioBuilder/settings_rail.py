5e54c784671b44d89b3de05b53e349509687d083
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2023-06-22 20:04:53 +0530

    added x forwarded host

--- PortfolioBuilder/PortfolioBuilder/settings_rail.py
+++ PortfolioBuilder/PortfolioBuilder/settings_rail.py
@@ -5,6 +5,7 @@ SECRET_KEY=config('SECRET_KEY')
 ALLOWED_HOSTS = ['portfoliobuilder-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://portfoliobuilder-production.up.railway.app']
 #SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = 'https://portfoliobuilder-production.up.railway.app/accounts/google/login/callback/'
+USE_X_FORWARDED_HOST = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
